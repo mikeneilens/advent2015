2610f4f573e351e25cef3526df3748dad2774a5c
Mike Neilens <mike.neilens@gmail.com>
2022-01-18 21:43:09 +0000

    day02

--- day02/src/main/kotlin/Main.kt
+++ day02/src/main/kotlin/Main.kt
@@ -1,23 +1,25 @@
+data class Box (val l:Int, val w:Int, val h:Int)
+
 fun paperNeeded(data: String): Int {
     val (side1, side2, side3) = getDimensions(data).calcSides()
     return 2 * (side1 + side2 + side3) + listOf(side1, side2, side3).minOf {it}
 }
 
-fun Triple<Int, Int, Int>.calcSides() = Triple(first * second, first * third, second * third)
+fun Box.calcSides() = Triple(l * w, l * h, w * h)
 
 fun getDimensions(data: String) =
-    Triple(data.split("x")[0].toInt(), data.split("x")[1].toInt(), data.split("x")[2].toInt())
+    Box(data.split("x")[0].toInt(), data.split("x")[1].toInt(), data.split("x")[2].toInt())
 
 fun partOne(data:List<String>):Int =
-    data.fold(0){acc, value -> acc + paperNeeded(value) }
+    data.fold(0){totalPaper, boxData -> totalPaper + paperNeeded(boxData) }
 
 fun ribbonNeeded(data: String): Int {
     val sides = getDimensions(data)
     return sides.shortestDistanceAroundItsSides() + sides.volume()
 }
-fun Triple<Int, Int, Int>.shortestDistanceAroundItsSides() = listOf(2 * (first + second), 2 * ( first + third), 2 * (second +  third) ).minOf {it}
-fun Triple<Int, Int, Int>.volume() = first * second * third
+fun Box.shortestDistanceAroundItsSides() = listOf(2 * (l + w), 2 * ( l + h), 2 * (w + h) ).minOf {it}
+fun Box.volume() = l * w * h
 
 fun partTwo(data:List<String>):Int =
-    data.fold(0){acc, value -> acc + ribbonNeeded(value) }
+    data.fold(0){totalRibbon, boxData -> totalRibbon + ribbonNeeded(boxData) }
 
