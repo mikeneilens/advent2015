e4e457de499765af4492c8c8500884c6a220e752
Mike Neilens <mike.neilens@gmail.com>
2022-01-23 19:31:41 +0000

    day10

--- day10/src/main/kotlin/Main.kt
+++ day10/src/main/kotlin/Main.kt
@@ -5,16 +5,17 @@ fun lookAndSay(chars: List<Char>): List<Char> {
    chars.forEach { char ->
       if (char == lastChar) count++
       else {
-         result.apply { add(count.toString()[0]); result.add(lastChar) }
+         result.apply { add(count.toSingleDigitChar()); result.add(lastChar) }
          count = 1
          lastChar = char
       }
    }
-   result.apply { add(count.toString()[0]); result.add(lastChar) }
+   result.apply { add(count.toSingleDigitChar()); result.add(lastChar) }
    return result
 }
 
-tailrec fun partOne(string:List<Char>, noOfTimesToProcess:Int = 1, noOfTimes:Int = 0 ):Int =
-   if (noOfTimes >= noOfTimesToProcess) string.size
-   else partOne(lookAndSay(string), noOfTimesToProcess, noOfTimes + 1)
+tailrec fun partOne(chars:List<Char>, noOfTimesToProcess:Int = 1, noOfTimes:Int = 0 ):Int =
+   if (noOfTimes >= noOfTimesToProcess) chars.size
+   else partOne(lookAndSay(chars), noOfTimesToProcess, noOfTimes + 1)
 
+fun Int.toSingleDigitChar() = toString()[0]
