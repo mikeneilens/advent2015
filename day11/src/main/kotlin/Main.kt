b830e7bbe81328fe15aa987863c8f664b0f3d4d1
Mike Neilens <mike.neilens@gmail.com>
2022-01-24 21:31:22 +0000

    day11

---
+++ day11/src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+val letters = ('a'..'z').filter{ letter -> letter !in listOf('i','l','o') }
+
+fun Char.nextLetter() = if (this == 'z') 'a' else letters[letters.indexOf(this) + 1]
+
+fun List<Char>.increment(tail:List<Char> = listOf()):List<Char> =
+    if (isNotEmpty() && last() != 'z') dropLast(1) + last().nextLetter() + tail
+    else {
+        if ( isNotEmpty() ) dropLast(1).increment(listOf('a') + tail)
+        else tail.map{'a'} // has overflowed so return a list of all 'a'
+    }
+
+fun List<Char>.containsAscendingLetters():Boolean {
+    (0 until size - 2).forEach{ index ->
+        val char = this[index]
+        if (char != 'y' && char != 'z' && this[index + 1] == char.nextLetter() && this[index + 2] == char.nextLetter().nextLetter()) return true
+    }
+    return false
+}
+
+fun List<Char>.containsTwoDifferentPairs():Boolean {
+    var index = 0
+    var noOfpairs = 0
+    while (index < size - 1) {
+        if (this[index] == this[index + 1]) {
+            if (++noOfpairs == 2) return true
+            index++
+        }
+        index++
+    }
+    return false
+}
+
+fun String.nextPasswrd():String {
+    var chars = toList().increment()
+    while (!(chars.containsAscendingLetters() && chars.containsTwoDifferentPairs())) {
+       chars = chars.increment()
+    }
+    return chars.joinToString("")
+}
---
+++ day11/src/test/kotlin/MainTest.kt
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class MainTest {
+    @Test
+    fun `letter after a is b`(){
+        assertEquals('b','a'.nextLetter())
+    }
+    @Test
+    fun `letter after z is a`(){
+        assertEquals('a','z'.nextLetter())
+    }
+    @Test
+    fun `incrememnting xx 5 times`() {
+        assertEquals("xy".toList(), "xx".toList().increment() )
+        assertEquals("xz".toList(), "xx".toList().increment().increment() )
+        assertEquals("ya".toList(), "xx".toList().increment().increment().increment() )
+        assertEquals("yb".toList(), "xx".toList().increment().increment().increment().increment() )
+    }
+    @Test
+    fun `incrememnting zz 2 times`() {
+        assertEquals("aa".toList(),"zz".toList().increment())
+        assertEquals("ab".toList(),"zz".toList().increment().increment())
+    }
+    @Test
+    fun `string contains three ascending letters`() {
+        assertTrue("abc".toList().containsAscendingLetters())
+        assertTrue("aabc".toList().containsAscendingLetters())
+        assertTrue("abca".toList().containsAscendingLetters())
+        assertTrue("abcdffaa".toList().containsAscendingLetters())
+        assertTrue("ghjaabcc".toList().containsAscendingLetters())
+        assertFalse("ayza".toList().containsAscendingLetters())
+    }
+    @Test
+    fun `string contains two different pairs`() {
+        assertTrue("aabcc".toList().containsTwoDifferentPairs())
+        assertTrue("aacc".toList().containsTwoDifferentPairs())
+        assertTrue("abcdffaa".toList().containsTwoDifferentPairs())
+        assertTrue("ghjaabcc".toList().containsTwoDifferentPairs())
+        assertFalse("aaa".toList().containsTwoDifferentPairs())
+    }
+    @Test
+    fun `next password with sample data`() {
+        assertEquals("abcdffaa", "abcdefgh".nextPasswrd())
+    }
+    @Test
+    fun `part one`() {
+        assertEquals("hepxxyzz", "hepxcrrq".nextPasswrd())
+    }
+    @Test
+    fun `part two`() {
+        assertEquals("heqaabcc", "hepxcrrq".nextPasswrd().nextPasswrd())
+    }
+
+}
