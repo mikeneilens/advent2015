3805a024492cf69d23f572b614475b7bb8062644
Mike Neilens <mike.neilens@gmail.com>
2022-01-29 12:52:10 +0000

    day 14

--- day14/src/main/kotlin/Main.kt
+++ day14/src/main/kotlin/Main.kt
@@ -1,16 +1,17 @@
-data class Reindeer(val name:String, val flySpeed:Int, val flyTime:Int, val restTime:Int)
+data class Reindeer(val name:String, val flySpeed:Int, val flyTime:Int, val restTime:Int) {
+
+    fun distanceTravelled(time:Int):Int {
+        val blocksOfFlyAndRest = flyTime + restTime
+        val numberOfBlocksFlown = time / blocksOfFlyAndRest
+        val incompleteTime = time - numberOfBlocksFlown * blocksOfFlyAndRest
+        return flySpeed * flyTime * numberOfBlocksFlown + minOf( flySpeed * incompleteTime , flySpeed * flyTime)
+    }
+}
 
 fun List<String>.parse():List<Reindeer> =
     map{line -> Reindeer(line.components[0], line.components[1].toInt(), line.components[2].toInt(), line.components[3].toInt()) }
 
 val String.components get() = listOf(split(" ")[0], split(" ")[3], split(" ")[6], split(" ")[13])
-
-fun Reindeer.distanceTravelled(time:Int):Int {
-    val blocksOfFlyAndRest = flyTime + restTime
-    val numberOfBlocksFlown = time / blocksOfFlyAndRest
-    val incompleteTime = time - numberOfBlocksFlown * blocksOfFlyAndRest
-    return flySpeed * flyTime * numberOfBlocksFlown + minOf( flySpeed * incompleteTime , flySpeed * flyTime)
-}
 
 fun partOne(data:List<String>, time:Int):Int = data.parse().maxOf { it.distanceTravelled(time) }
 
@@ -23,13 +24,13 @@ fun List<Reindeer>.winnersAfterTime(time:Int):List<ReindeerTravel> {
 }
 
 fun List<Reindeer>.accumulatePoints(time:Int):Map<String, Int> {
-    val scores = mutableMapOf<String, Int>()
-    (1..time).forEach { t -> updateScoreOfWinners(t, scores) }
-    return scores
+    val scoresMap = associate { Pair(it.name, 0) }.toMutableMap()
+    (1..time).forEach { t -> updateScoreOfWinners(t, scoresMap) }
+    return scoresMap
 }
 
 private fun List<Reindeer>.updateScoreOfWinners(time: Int, scores: MutableMap<String, Int>) {
-    winnersAfterTime(time).forEach { winner -> scores[winner.name] = scores.getOrDefault(winner.name, 0) + 1 }
+    winnersAfterTime(time).forEach { winner -> scores[winner.name] = scores.getValue(winner.name) + 1 }
 }
 
 fun partTwo(data:List<String>, time:Int):Int = data.parse().accumulatePoints(time).maxOf { it.value }
