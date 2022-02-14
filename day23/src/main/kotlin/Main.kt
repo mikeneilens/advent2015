eabc0b3c7317043da37538f1a39d9975a66586aa
Mike Neilens <mike.neilens@gmail.com>
2022-02-14 23:01:57 +0000

    day 23

--- day23/src/main/kotlin/Main.kt
+++ day23/src/main/kotlin/Main.kt
@@ -1,35 +1,56 @@
 
-data class ComputerStatus(val step:Int = 0, val a:Int = 0, val b:Int)
+val hlf = { computerStatus:ComputerStatus, param1:String, _:String ->
+    ComputerStatus(computerStatus.step + 1,
+        if (param1 == "a") computerStatus.a / 2 else computerStatus.a,
+        if (param1 == "b") computerStatus.b / 2 else computerStatus.b)}
+
+val  tpl = {computerStatus:ComputerStatus, param1:String, _:String  ->
+    ComputerStatus(computerStatus.step + 1,
+        if (param1 == "a") computerStatus.a *3 else computerStatus.a,
+        if (param1 == "b") computerStatus.b * 3 else computerStatus.b  )}
+
+val inc ={computerStatus:ComputerStatus, param1:String, _:String ->
+    ComputerStatus(computerStatus.step + 1,
+        if (param1 == "a") computerStatus.a + 1 else computerStatus.a,
+        if (param1 == "b") computerStatus.b + 1 else computerStatus.b  )}
+
+val jmp = {computerStatus:ComputerStatus, param1:String, _:String ->
+    ComputerStatus(computerStatus.step + param1.toInt(),
+        computerStatus.a,
+        computerStatus.b )}
+
+val jie = { computerStatus:ComputerStatus, param1:String, param2:String ->
+    if (param1 == "a" && (computerStatus.a % 2) == 0 || param1 == "b" && (computerStatus.b % 2) == 0)
+        ComputerStatus(computerStatus.step + param2.toInt(), computerStatus.a, computerStatus.b )
+    else ComputerStatus(computerStatus.step + 1, computerStatus.a, computerStatus.b )}
+
+val jio = { computerStatus:ComputerStatus, param1:String, param2:String ->
+    if (param1 == "a" && computerStatus.a  == 1 || param1 == "b" && computerStatus.b  == 1)
+        ComputerStatus(computerStatus.step + param2.toInt(), computerStatus.a, computerStatus.b )
+    else ComputerStatus(computerStatus.step + 1, computerStatus.a, computerStatus.b )}
+
+val instructions = mapOf("hlf" to hlf, "tpl" to tpl, "inc" to inc, "jmp" to jmp, "jie" to jie, "jio" to jio)
+
+data class ComputerStatus(val step:Int = 0, val a:Int = 0, val b:Int = 0)
 
 fun String.process(computerStatus: ComputerStatus):ComputerStatus {
-    val instruction = toInstruction()
-    val param = split(" ")[1].removeSuffix(",")
-    val offset = if (split(" ").size > 2) split(" ")[2].toInt() else 0
-    return instruction(computerStatus, param, offset  )
+    val instruction = instructions.getValue(split(" ").first())
+    val param1 = split(" ")[1].removeSuffix(",")
+    val param2 = if (split(" ").size > 2) split(" ")[2] else ""
+    return instruction(computerStatus, param1, param2  )
 }
 
-fun String.toInstruction() = instructions.getValue(split(" ").first())
+class Program(private val data:List<String>) {
 
-val instructions = mapOf("hlf" to ::hlf, "tpl" to ::tpl, "inc" to ::inc, "jmp" to ::jmp, "jie" to ::jie, "jio" to ::jio)
+    fun run(initialStatus:ComputerStatus = ComputerStatus()):ComputerStatus  {
+        var computerStatus = initialStatus
+        while (computerStatus.step < data.size) {
+            computerStatus = data[computerStatus.step].process(computerStatus)
+        }
+        return computerStatus
+    }
+}
 
-fun hlf(computerStatus:ComputerStatus, register:String, offset:Int) =
-    ComputerStatus(computerStatus.step + 1, if (register == "a") computerStatus.a /2 else computerStatus.a, if (register == "b") computerStatus.b /2 else computerStatus.b  )
+fun partOne(data:List<String>):Int = Program(data).run().b
 
-fun tpl(computerStatus:ComputerStatus, register:String, offset:Int) =
-    ComputerStatus(computerStatus.step + 1, if (register == "a") computerStatus.a *3 else computerStatus.a, if (register == "b") computerStatus.b * 3 else computerStatus.b  )
-
-fun inc(computerStatus:ComputerStatus, register:String, offset:Int) =
-    ComputerStatus(computerStatus.step + 1, if (register == "a") computerStatus.a + 1 else computerStatus.a, if (register == "b") computerStatus.b + 1 else computerStatus.b  )
-
-fun jmp(computerStatus:ComputerStatus, register:String, offset:Int) =
-    ComputerStatus(computerStatus.step + register.toInt(), computerStatus.a, computerStatus.b )
-
-fun jie(computerStatus:ComputerStatus, register:String, offset:Int) =
-    if (register == "a" && (computerStatus.a % 2) == 0 || register == "b" && (computerStatus.b % 2) == 0)
-        ComputerStatus(computerStatus.step + offset, computerStatus.a, computerStatus.b )
-    else computerStatus
-
-fun jio(computerStatus:ComputerStatus, register:String, offset:Int) =
-    if (register == "a" && computerStatus.a  == 1 || register == "b" && computerStatus.b  == 1)
-        ComputerStatus(computerStatus.step + offset, computerStatus.a, computerStatus.b )
-    else computerStatus
+fun partTwo(data:List<String>):Int = Program(data).run(ComputerStatus(0,1,0)).b
--- day23/src/test/kotlin/MainTest.kt
+++ day23/src/test/kotlin/MainTest.kt
@@ -2,6 +2,13 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class MainTest {
+    private val sampleData = """
+        inc a
+        jio a, +2
+        tpl a
+        inc a
+    """.trimIndent().split("\n")
+
     @Test
     fun `hlf r sets register r to half its current value, then continues with the next instruction`() {
         val computerStatus = ComputerStatus(0,4,7)
@@ -29,14 +36,30 @@ class MainTest {
     @Test
     fun `jie r, offset is like jmp, but only jumps if register r is even`() {
         val computerStatus = ComputerStatus(0,4,7)
-        assertEquals(ComputerStatus(1, 4,7), "jie a, +1".process(computerStatus) )
-        assertEquals(ComputerStatus(0, 4,7), "jie b, +1".process(computerStatus) )
+        assertEquals(ComputerStatus(2, 4,7), "jie a, +2".process(computerStatus) )
+        assertEquals(ComputerStatus(1, 4,7), "jie b, +2".process(computerStatus) )
     }
     @Test
     fun `jio r, offset is like jmp, but only jumps if register r is 1`() {
         val computerStatus = ComputerStatus(0,1,7)
-        assertEquals(ComputerStatus(1, 1,7), "jio a, +1".process(computerStatus) )
-        assertEquals(ComputerStatus(0, 1,7), "jio b, +1".process(computerStatus) )
+        assertEquals(ComputerStatus(2, 1,7), "jio a, +2".process(computerStatus) )
+        assertEquals(ComputerStatus(1, 1,7), "jio b, +2".process(computerStatus) )
+    }
+    @Test
+    fun `program using sample data`() {
+        val computerStatus = Program(sampleData).run()
+        assertEquals(2, computerStatus.a)
+        assertEquals(0, computerStatus.b)
+    }
+
+    @Test
+    fun `part one`() {
+        assertEquals(307, partOne(puzzleInput))
+    }
+
+    @Test
+    fun `part two`() {
+        assertEquals(160, partTwo(puzzleInput))
     }
 
 }
