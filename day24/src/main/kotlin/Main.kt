3d8d49710e3ded21bface965bd704442aaa7a08c
Mike Neilens <mike.neilens@gmail.com>
2022-02-15 23:56:46 +0000

    day 24

--- day24/src/main/kotlin/Main.kt
+++ day24/src/main/kotlin/Main.kt
@@ -6,16 +6,13 @@ fun quantumEntanglement(set:Set<Int>) = set.fold(1L){ acc, i -> acc * i  }
 data class Info(var smallestGroupSize:Int = Int.MAX_VALUE)
 
 fun groupsWithTargetWeight(targetWeight:Int, fullSet:List<Int>, setSoFar:Set<Int>, info:Info = Info()):List<Set<Int>> {
-    if (setSoFar.weight() == targetWeight){
+    return if (setSoFar.weight() == targetWeight){
         info.smallestGroupSize = setSoFar.size
-        return listOf(setSoFar)
-    }
-
-    return if (setSoFar.size < info.smallestGroupSize)
-        fullSet.filter{it + setSoFar.weight() <= targetWeight}.flatMap { num ->
-            groupsWithTargetWeight(targetWeight, fullSet.filter{it < num} , setSoFar + num, info )
-        }
-    else listOf()
+        listOf(setSoFar)
+    } else fullSet
+        .filter{ num -> (setSoFar.size < info.smallestGroupSize) && (num + setSoFar.weight() <= targetWeight)}
+        .flatMap { num ->
+            groupsWithTargetWeight(targetWeight, fullSet.filter{it < num} , setSoFar + num, info )}
 }
 
 val targetWeightPartOne = { strings:List<String> -> strings.sumOf(String::toInt)/3}
@@ -24,8 +21,8 @@ fun parse(data:List<String>) = data.map(String::toInt).reversed()
 
 fun partOne(data:List<String>, weightCalculator:(List<String> )->Int = targetWeightPartOne  ):Long {
     val groupsMatchingTarget =  groupsWithTargetWeight(weightCalculator(data), parse(data), setOf())
-    val groupsWithSmallestSize = { group:Set<Int> -> group.size == groupsMatchingTarget.minByOrNull{ it.size }?.size}
-    return groupsMatchingTarget.filter(groupsWithSmallestSize)
+    val groupsWithSmallestSize = groupsMatchingTarget.minByOrNull{ it.size }?.size
+    return groupsMatchingTarget.filter {group -> group.size == groupsWithSmallestSize  }
         .map(::quantumEntanglement)
         .minOf{ it }
 }
